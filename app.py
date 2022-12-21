bf08a7fda731328b035f9e9b0942399972c75604
maksimcialehin <madqfrog1863@gmail.com>
2022-12-21 12:15:39 +0300

    first queries

--- app.py
+++ app.py
@@ -1,17 +1,42 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
+from model.dbHandler import match_exact, match_like
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    response = request.args.getlist('word')
+    return response
 
 
 @app.route('/search')
 def search():
-    args = request.args.get('q')
-    return redirect(f'https://www.google.com/search?q={args}')
+    return render_template('index.html')
+
+
+@app.route('/dict')
+def dictionary():
+    words = request.args.getlist('word')
+
+    if not words:
+        return jsonify({'word': words, 'status': 'error', 'data': 'not found'})
+    
+    response = {'words': []}
+
+    for word in words:
+        definitions = match_exact(word)
+
+        if definitions:
+            response['words'].append({'word': word, 'status': 'success', 'data': definitions})
+        else:
+            definitions = match_like(word)
+            if definitions:
+                response['words'].append({'word': word, 'status': 'partial', 'data': definitions})
+            else:
+                response['words'].append({'word': word, 'success': 'error', 'data': 'not found'})
+    
+    return jsonify(response)
 
 
 if __name__ == '__main__':
---
+++ model/dbHandler.py
@@ -0,0 +1,21 @@
+import sqlite3 as SQL
+
+
+def match_exact(word: str) -> list:
+    db = SQL.connect('data/dictionary.db')
+
+    sql_query = 'SELECT * from entries WHERE word=?'
+    match = db.execute(sql_query, (word,)).fetchall()
+
+    db.close()
+    return match
+
+
+def match_like(word: str) -> list:
+    db = SQL.connect('data/dictionary.db')
+
+    sql_query = 'SELECT * from entries WHERE word LIKE ?'
+    match = db.execute(sql_query, (word,)).fetchall()
+
+    db.close()
+    return match
